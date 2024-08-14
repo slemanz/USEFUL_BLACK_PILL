74aa255e43db40756daf81ad54b3d826e21f8b3c
slemanz <william_sleman@hotmail.com>
2024-08-14 20:52:19 -0300

    project(feat): add blinky example

---
+++ BLINKY/Inc/command.h
@@ -0,0 +1,31 @@
+/*
+ * command.h
+ *
+ *  Created on: Jul 29, 2024
+ *      Author: sleman
+ */
+
+#ifndef COMMAND_H_
+#define COMMAND_H_
+
+#include "stm32f401.h"
+
+// Function pointer type
+typedef void(*function_t)(void);
+
+// Structure to hold command information
+typedef struct
+{
+    char const *name;
+    function_t execute;
+    char const *help;
+}commandStruct_t;
+
+void ProcessCommands(void);
+
+// Function prototypes for command handlers
+void TurnOnLED(void);
+void TurnOffLED(void);
+void Help(void);
+
+#endif /* COMMAND_H_ */
