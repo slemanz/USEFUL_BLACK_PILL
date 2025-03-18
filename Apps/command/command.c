7ffaa7b63ee3b0bafbe1e7c6da108f2d22abe8cf
slemanz <william_sleman@hotmail.com>
2025-03-18 04:41:20 -0300

    project(test): serial working fine

--- Apps/command/command.c
+++ Apps/command/command.c
@@ -1,27 +1,19 @@
 #include "common-defines.h"
 #include "init.h"
 #include "ticks.h"
+#include "serial.h"
 
-uint8_t ch = 0;
-
-void USART2_IRQHandler (void)
-{
-    const bool overrun_occurred = UART_GetFlagStatus(UART2, UART_FLAG_ORE);
-	const bool received_data = UART_GetFlagStatus(UART2, UART_FLAG_RXNE);
-
-	if(received_data || overrun_occurred)
-	{
-        ch = UART2->DR;
-	}
-}
 
 int main(void)
 {
     init_io();
     init_systick();
     init_serial();
+    
+    serial_setup();
 
     uint64_t start_time = ticks_get();
+    uint8_t ch = 0;
 
     while (1)
     {
@@ -31,10 +23,10 @@ int main(void)
             start_time = ticks_get();
         }
 
-		if(ch != 0)
+		if(serial_data_available())
 		{
-            uart_write(UART2, &ch, 1);
-            ch = 0;
+            ch = serial_read_byte();
+            serial_send(&ch, 1);
 		}
     }
 }
